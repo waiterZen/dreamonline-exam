fe18a05c091ba941cbc1a9b4bf21cb7d0da0dd0d
Xiao Jian'gang <upupxjg@gmail.com>
2012-12-17 23:00:10 +0800

    create maven struct

---
+++ student-parent/student-core/src/main/java/org/student/core/App.java
@@ -0,0 +1,13 @@
+package org.student.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
